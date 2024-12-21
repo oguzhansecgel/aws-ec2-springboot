3f0753dca69847f076a0f662283c03a1cfe576a6
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-12-21 22:44:58 +0300

    jenkins

--- src/main/java/com/aws/aws_deployment/controller/MessageController.java
+++ src/main/java/com/aws/aws_deployment/controller/MessageController.java
@@ -1,11 +1,13 @@
 package com.aws.aws_deployment.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
@@ -15,9 +17,34 @@ public class MessageController {
     {
             return ResponseEntity.ok("Aws Deployment Successfully Completed");
     }
+    @GetMapping("/dev")
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("Oğuzhan SEÇGEL");
+    }
     @GetMapping("/name")
     public ResponseEntity<String> getName(@RequestParam String name)
     {
         return ResponseEntity.ok(String.format("Hello %s",name));
+    }
+}
+*/
+@Controller
+public class MessageController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+    @GetMapping("/dev")
+    public String getDev() {
+        return "dev";
+    }
+
+    @GetMapping("/name")
+    public String getName(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
